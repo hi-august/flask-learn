3a749274d81b83f441101f8f0ec33c6cd265b520
augustles <1927064778@qq.com>
2017-03-27 13:59:54 +0800

    update

--- app/blog/models.py
+++ app/blog/models.py
@@ -5,7 +5,7 @@ from datetime import datetime as dte
 
 class User(db.Model):
     __tablename__ = 'user'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20))
 
     def __unicode__(self):
@@ -19,7 +19,7 @@ class User(db.Model):
 # 添加记录 Book(name='hi', user=User(username='august')
 class Book(db.Model):
     __tablename__ = 'book'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20))
     # 外键的字段
     user_id = db.Column(
@@ -38,7 +38,7 @@ class Book(db.Model):
 
 
 class Category(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
 
     def __init__(self, name):
@@ -48,17 +48,25 @@ class Category(db.Model):
         return '<Category %r>' % self.name
 
 class Comment(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200))
     pub_date = db.Column(db.DateTime, default=dte.now())
 
     def __repr__(self):
         return '<Comment %r>' % self.name
 
+class Tag(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+
+    def __repr__(self):
+        return '<Tag %r>' % self.name
+
 class Post(db.Model):
     """ 定义了五个字段，分别是 id，title，body，pub_date，category_id
     """
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    __tablename__ = 'post'
+    id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(80))
     content = db.Column(db.Text)
     pub_date = db.Column(db.DateTime, default=dte.now())
@@ -70,6 +78,9 @@ class Post(db.Model):
 
     comment_id = db.Column(db.Integer, db.ForeignKey('comment.id'))
     comment = db.relationship('Comment', backref=db.backref('post_set', lazy='dynamic'))
+
+    tag_id = db.Column(db.Integer, db.ForeignKey('tag.id'))
+    tag = db.relationship('Tag', backref=db.backref('post_set', lazy='dynamic'))
 
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     user = db.relationship('User', backref=db.backref('post_set', lazy='dynamic'))
@@ -153,10 +164,16 @@ class MysqlJianshu(db.Model):
 
 
 def init_db():
+    # 创建数据表
     db.create_all()
 
 def drop_db():
+    # 删除数据表
     db.drop_all()
+
+def reset():
+    # 回滚还原之前数据库状态
+    db.session.rollback()
 
 def save(p):
     # add_all添加列表 add()
@@ -164,6 +181,19 @@ def save(p):
     # Book.query.filter().update() b.name = 'war and peace' save(b)
     # Book.query.filter_by().all() 返回一个列表
     # first()
-    db.session.add(p)
-    res = db.session.commit()
-    return res
+    # session是为了保证数据库的一致性,
+    # 提交数据使用原子方式把会话对象写入数据库
+    # 事务用来处理量大,复杂度高的数据
+    # 数据库事务具有以下四个特性
+    # 1.原子性,一个事务的所有操作,要么全部完成,要么全部不完成
+    # 2. 一致性(稳定性),有非法数据(外键约束),事务返回
+    # 3. 隔离性,事务独立运行,一个事务如果影响到其他事务,其他事务会撤回
+    # 4. 持久性,软硬件崩溃,innodb会根据日志进行重构修改
+
+    try:
+        db.session.add(p)
+    except Exception as e:
+        print(e)
+        db.session.rollback()
+    else:
+        db.session.commit()
